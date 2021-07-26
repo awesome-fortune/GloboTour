4b969904bd6722b47ceaaa13eb31e87d2bd1ef73
Fortune Maseko <mrfortune.maseko@gmail.com>
2021-07-26 14:30:15 +0200

    Implement bottom navigation

--- app/src/main/java/za/co/masekofortune/globotour/MainActivity.kt
+++ app/src/main/java/za/co/masekofortune/globotour/MainActivity.kt
@@ -2,21 +2,19 @@ package za.co.masekofortune.globotour
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
-import com.google.android.material.navigation.NavigationView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var toolbar: MaterialToolbar
     private lateinit var navController: NavController
-    private lateinit var navigationView: NavigationView
-    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var bottomNavView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,29 +22,21 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize Views
         toolbar = findViewById(R.id.activity_main_toolbar)
-        navigationView = findViewById(R.id.nav_view)
-        drawerLayout = findViewById(R.id.drawer_layout)
+        bottomNavView = findViewById(R.id.bottom_nav_view)
 
         // Get NavHostFragment and NavController
         val navHostFrag =
             supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
         navController = navHostFrag.navController
 
-        // Define AppBarConfiguration: Connect DrawerLayout with Navigation Component
-        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        // Define AppBarConfiguration
+        val topLevelDestinations = setOf(R.id.fragmentCityList, R.id.fragmentFavoriteList)
+        val appBarConfiguration = AppBarConfiguration(topLevelDestinations)
 
         // Connect Toolbar with NavController
         toolbar.setupWithNavController(navController, appBarConfiguration)
 
         // Connect NavigationView with NavController
-        navigationView.setupWithNavController(navController)
-    }
-
-    override fun onBackPressed() {
-        if (drawerLayout.isOpen) {
-            drawerLayout.close()
-        } else {
-            super.onBackPressed()
-        }
+        bottomNavView.setupWithNavController(navController)
     }
 }
