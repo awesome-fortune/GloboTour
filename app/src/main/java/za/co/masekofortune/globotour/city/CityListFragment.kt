03abd6cf435971fbe7f6528e5d9b141d43594d08
Fortune Maseko <mrfortune.maseko@gmail.com>
2021-07-26 12:21:24 +0200

    Remove unused imports

--- app/src/main/java/za/co/masekofortune/globotour/city/CityListFragment.kt
+++ app/src/main/java/za/co/masekofortune/globotour/city/CityListFragment.kt
@@ -5,10 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import za.co.masekofortune.globotour.R
 
 
