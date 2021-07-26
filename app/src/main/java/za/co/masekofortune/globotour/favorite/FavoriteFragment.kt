d998c1053def2f243c29896e71c346e730a26c11
Fortune Maseko <mrfortune.maseko@gmail.com>
2021-07-26 13:06:48 +0200

    Add app screenshots

--- app/src/main/java/za/co/masekofortune/globotour/favorite/FavoriteFragment.kt
+++ app/src/main/java/za/co/masekofortune/globotour/favorite/FavoriteFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,24 +14,38 @@ import za.co.masekofortune.globotour.city.VacationSpots
 
 
 class FavoriteFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var textView: TextView
+    private lateinit var favoriteCityList: ArrayList<City>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val view = inflater.inflate(R.layout.fragment_favorite, container, false)
 
-        setupRecyclerView(view)
+        favoriteCityList = VacationSpots.favoriteCityList as ArrayList<City>
+
+        recyclerView = view.findViewById(R.id.favorite_recycler_view)
+        textView = view.findViewById(R.id.tvNoFavorites)
+
+        if (favoriteCityList.isEmpty()) {
+            showEmptyDataAlert()
+        } else {
+            setupRecyclerView()
+        }
 
         return view
     }
 
-    private fun setupRecyclerView(view: View) {
+    private fun showEmptyDataAlert() {
+        textView.visibility = View.VISIBLE
+    }
 
+    private fun setupRecyclerView() {
         val context = requireContext()
 
         val favoriteCityList = VacationSpots.favoriteCityList as ArrayList<City>
         val favoriteAdapter = FavoriteAdapter(context, favoriteCityList)
 
-        val recyclerView = view.findViewById<RecyclerView>(R.id.favorite_recycler_view)
         recyclerView.adapter = favoriteAdapter
         recyclerView.setHasFixedSize(true)
 
